9661e1d1d188943094ed769d105a00233ae47e79
한찬희 <140091899+HanChanHui@users.noreply.github.com>
2024-06-03 16:13:58 +0900

    09. Resource에서 icon 호출

--- MapCreate/Assets/Scripts/EditorWindow/Editor/MapEditorsEditor.cs
+++ MapCreate/Assets/Scripts/EditorWindow/Editor/MapEditorsEditor.cs
@@ -11,6 +11,7 @@ public class MapEditorsEditor : Editor
     public VisualTreeAsset TreeAsset;
     private MapEditors _mapEditors;
 
+    // ===============================================
     public VisualElement gridiconPanel;
     public BaseField<int> widthField;
     public BaseField<int> heightField;
@@ -20,10 +21,6 @@ public class MapEditorsEditor : Editor
     public Label result;
 
     public Toggle spritefullin;
-
-    //public List<Button> gridButtons = new List<Button>();
-
-    public int iconEventCnt;
 
     public override VisualElement CreateInspectorGUI()
     {
@@ -35,17 +32,18 @@ public class MapEditorsEditor : Editor
         VisualElement root = new VisualElement();
         TreeAsset.CloneTree(root);
         _mapEditors.Init();
+        
 
         // Add your UI content here
         var inputMScript = root.Q<ObjectField>("unity-input-m_Script");
         inputMScript.AddToClassList("unity-disabled");
         inputMScript.Q(null, "unity-object-field__selector")?.SetEnabled(false);
-        // root.Q<Label>("title").text = "Custom Property Drawer";
-        // root.Q<Button>("btn").clickable.clicked += () => Debug.Log("Clicked!");
+
 
         widthField = root.Q<BaseField<int>>("width_int");
         heightField = root.Q<BaseField<int>>("height_int");
         gridiconPanel = root.Q<VisualElement>("gridiconPanel");
+        _mapEditors.IconLoad(gridiconPanel);
 
         // 현재 레벨 인덱스
         currentLevelField = root.Q<BaseField<int>>("current_level");
@@ -58,44 +56,22 @@ public class MapEditorsEditor : Editor
         result = root.Q<Label>("result_label");
         
 
-        root.Q<Button>("grid_add").clicked += ()=> { _mapEditors.IconAdd(gridiconPanel, iconEventCnt); };
+        root.Q<Button>("grid_add").clicked += ()=> { _mapEditors.IconAdd(gridiconPanel); };
         root.Q<Button>("createBtn").clicked += ()=> { _mapEditors.BtnCreate(spritefullin, result, widthField.value, heightField.value); };
         root.Q<Button>("deleteBtn").clicked += ()=> { _mapEditors.BtnClose(); };
         root.Q<Button>("saveBtn").clicked += ()=> { _mapEditors.SaveBtn(fileName, result, widthField.value, heightField.value); };
         root.Q<Button>("loadBtn").clicked += ()=> { _mapEditors.LoadBtn(fileName, result, currentLevelField, spritefullin); };
         root.Q<Button>("iconrotationBtn").clicked += () => { _mapEditors.IconRotate(); };
 
-        iconEventCnt = 0;
-        if(gridiconPanel != null)
-        {
-            // grid 버튼 등록 및 이벤트 추가
-            foreach(var child in gridiconPanel.Children())
-            {
-                if(child is Button button)
-                {
-                    if(button.name != "grid_add")
-                    {
-                        _mapEditors.iconData[iconEventCnt].btn = button;
-                        _mapEditors.iconData[iconEventCnt++].btn.RegisterCallback<ClickEvent>(evt => _mapEditors.OnGridCheckButton(evt, iconEventCnt));
-                    }
-                }
-            }
-        }
         
+        _mapEditors.IconEvent(gridiconPanel);
         
         return root;
     }
 
     private void OnDisable()
     {
-        for(int i = 0; i < iconEventCnt - 1; i++)
-        {
-            _mapEditors.iconData[i].btn.UnregisterCallback<ClickEvent>(evt => _mapEditors.OnGridCheckButton(evt, iconEventCnt));
-        }
-
-        if(_mapEditors != null)
-        {
-            _mapEditors.BtnClose();
-        }
+       _mapEditors.IconEventDelete();
     }
+
 }
--- MapCreate/Assets/Scripts/EditorWindow/MapEditors.cs
+++ MapCreate/Assets/Scripts/EditorWindow/MapEditors.cs
@@ -4,17 +4,22 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 using Newtonsoft.Json;
+using System.Text;
 
 class Map{
-    public string name = new string("");
+    public string name;
     public int lv;
-    public int width = 0;
-    public int height = 0;
-    public int[,] idx = new int[0,0];
+    public int width;
+    public int height;
+    public int[,] idx;
 
     public Map()
     {
-
+        this.name = new string("");
+        this.lv = 0;
+        this.width = 0;
+        this.height = 0;
+        this.idx = new int[0,0];
     }
 }
 
@@ -22,7 +27,7 @@ public class MapEditors : MonoBehaviour
 {
    
 
-        /// <summary>
+    /// <summary>
     /// 맵 정보를 담을 버튼
     /// </summary>
     public ButtonData[] buttons = new ButtonData[100];
@@ -42,10 +47,13 @@ public class MapEditors : MonoBehaviour
     public int selecticonRotate;
 
     /// <summary>
-    /// Icon 갯수
+    /// Icon 갯수 / IconEvent 갯수
     /// </summary>
     public int iconCnt;
-
+    public int iconEventCnt;
+    /// <summary>
+    /// Grid Window
+    /// </summary>
     public MapWindow window;
     public VisualElement btnTool;
 
@@ -55,31 +63,74 @@ public class MapEditors : MonoBehaviour
         currentLevel = 0;
         selectInt = 0;
         selecticonRotate = 0;
+        iconEventCnt = 0;
         iconCnt = 0;
-        IconLoad();
+    }
+    public void IconEvent(VisualElement _gridiconPanel)
+    {
+        if(_gridiconPanel != null)
+        {
+            // grid 버튼 등록 및 이벤트 추가
+            foreach(var child in _gridiconPanel.Children())
+            {
+                if(child is Button button)
+                {
+                    if(button.name != "grid_add")
+                    {
+                        iconData[iconEventCnt].btn = button;
+                        iconData[iconEventCnt++].btn.RegisterCallback<ClickEvent>(OnGridCheckButton);
+                    }
+                }
+            }
+        }
     }
 
-    public void IconLoad()
+    public void IconEventDelete()
+    {
+        for(int i = 0; i < iconEventCnt - 1; i++)
+        {
+            iconData[i].btn.UnregisterCallback<ClickEvent>(OnGridCheckButton);
+        }
+
+        BtnClose();
+    }
+
+    public void IconLoad(VisualElement _panel)
     {
         iconData[iconCnt].icon = null;
         iconData[iconCnt++].rotate = selecticonRotate;
-        iconData[iconCnt].icon = Resources.Load<Sprite>("Sprite/Tile 0");
-        iconData[iconCnt++].rotate = selecticonRotate;
-        iconData[iconCnt].icon = Resources.Load<Sprite>("Sprite/Tile 1");
-        iconData[iconCnt++].rotate = selecticonRotate;
-        iconData[iconCnt].icon = Resources.Load<Sprite>("Sprite/Tile 2");
-        iconData[iconCnt++].rotate = selecticonRotate;
-        iconData[iconCnt].icon = Resources.Load<Sprite>("Sprite/Tile 3");
-        iconData[iconCnt++].rotate = selecticonRotate;
-        iconData[iconCnt].icon = Resources.Load<Sprite>("Sprite/Tile 4");
-        iconData[iconCnt++].rotate = selecticonRotate;
-        iconData[iconCnt].icon = Resources.Load<Sprite>("Sprite/Tile 5");
-        iconData[iconCnt++].rotate = selecticonRotate;
+
+        Sprite[] sprites = Resources.LoadAll<Sprite>("Sprite");
+        
+        if(sprites.Length > 0)
+        {
+            for(int i = 0; i < sprites.Length; i++)
+            {
+                iconData[iconCnt].icon = sprites[i];
+                iconData[iconCnt++].rotate = 0;
+
+                // Icon 추가
+                Button btn = new Button();
+                btn.style.width = 50;
+                btn.style.height = 50;
+                btn.name = "grid_" + iconCnt.ToString();
+
+                _panel.Add(btn);
+                btn.AddToClassList("button-grid");
+                btn.tabIndex = iconCnt - 1;
+                btn.style.backgroundImage = new StyleBackground(iconData[iconCnt - 1].icon);
+            }
+        }
+        else
+        {
+            Debug.Log("스프라이트가 없습니다.");
+        }
+
     }
     /// <summary>
     /// icon 파일 불러오기
     /// </summary>
-    public void IconAdd(VisualElement _panel,  int _iconEventCnt)
+    public void IconAdd(VisualElement _panel)
     {
         var path = EditorUtility.OpenFilePanel("Open icon", Application.dataPath + 
                     "/Resources/sprite", "asset");
@@ -87,13 +138,13 @@ public class MapEditors : MonoBehaviour
         if(!string.IsNullOrEmpty(path))
         {
             string iconname = filename.Substring(0, filename.IndexOf('.'));
-            IconCreate(_panel, iconname, _iconEventCnt);    
+            IconCreate(_panel, iconname);    
         }
     }
     /// <summary>
     /// icon 등록
     /// </summary>
-    public void IconCreate(VisualElement _panel, string _name, int _iconEventCnt)
+    public void IconCreate(VisualElement _panel, string _name)
     {
         iconData[iconCnt].icon = Resources.Load<Sprite>("Sprite/" + _name);
         iconData[iconCnt++].rotate = 0;
@@ -105,9 +156,9 @@ public class MapEditors : MonoBehaviour
         btn.name = "grid_" + iconCnt.ToString();
 
         _panel.Add(btn);
-        iconData[_iconEventCnt].btn = btn;
+        iconData[iconEventCnt].btn = btn;
 
-        iconData[_iconEventCnt++].btn.RegisterCallback<ClickEvent>(evt => OnGridCheckButton(evt, _iconEventCnt));
+        iconData[iconEventCnt++].btn.RegisterCallback<ClickEvent>(OnGridCheckButton);
         btn.AddToClassList("button-grid");
         btn.tabIndex = iconCnt - 1;
         btn.style.backgroundImage = new StyleBackground(iconData[iconCnt - 1].icon);
@@ -145,14 +196,14 @@ public class MapEditors : MonoBehaviour
     /// <summary>
     /// 어떤 icon grid 버튼을 선택 했는지
     /// </summary>
-    public void OnGridCheckButton(ClickEvent _evt, int _maxCnt)
+    public void OnGridCheckButton(ClickEvent _evt)
     {
         var target = _evt.target as VisualElement;
 
         // 초기화
         if(!target.ClassListContains("button-grid--check"))
         {
-            for(int i = 0; i < _maxCnt; i++)
+            for(int i = 0; i < iconCnt; i++)
             {
                 if(iconData[i].btn.ClassListContains("button-grid--check"))
                 {
@@ -162,7 +213,7 @@ public class MapEditors : MonoBehaviour
 
             // 선택
             target.AddToClassList("button-grid--check");
-            // TODO 회전값을 받아서 적용하게끔 적용.
+
             selectInt = target.tabIndex;
             selecticonRotate = iconData[selectInt].rotate;
         }
@@ -170,6 +221,7 @@ public class MapEditors : MonoBehaviour
         {
             // 취소
             target.RemoveFromClassList("button-grid--check");
+
             selectInt = 0;
             selecticonRotate = 0;
         }  
@@ -189,12 +241,11 @@ public class MapEditors : MonoBehaviour
         
         WindowCreate(_toggle, _width, _height);
         
-        
         _result.text = "생성 완료";
     }
 
     /// <summary>
-    /// Map Window 창 생성
+    /// Map Window 창 생성.
     /// </summary>
     private void WindowCreate(Toggle _toggle, int _width, int _height)
     {
@@ -210,7 +261,9 @@ public class MapEditors : MonoBehaviour
 
         WindowBtnCreate(btnTool, _toggle, _width, _height);
     }
-
+    /// <summary>
+    /// Map Window 창에 버튼 생성.
+    /// </summary>
     private void WindowBtnCreate(VisualElement _btnTool, Toggle _toggle, int _width, int _height)
     {
         int maxRange = BtnToolPanelSize(_btnTool, _width, _height);
@@ -224,13 +277,12 @@ public class MapEditors : MonoBehaviour
             _btnTool.Add(buttons[i].btn);
 
             buttons[i].btn.RegisterCallback<ClickEvent>(evt => {BtnClick(evt, _toggle, maxRange);});
-            //buttons[i].btn.clickable.clickedWithEventInfo += BtnClick;
             buttons[i].btn.style.backgroundImage = new StyleBackground(iconData[0].icon);
         }
     }
 
     /// <summary>
-    /// Panel Size 조절
+    /// Panel Size 조절.
     /// </summary>
     public int BtnToolPanelSize(VisualElement _btnTool, int _width, int _height)
     {
@@ -247,14 +299,16 @@ public class MapEditors : MonoBehaviour
     public void BtnClose()
     {
         if(window == null)
+        {
             return;
+        }
         btnTool.Clear();
         window.Close();
 
         buttons = new ButtonData[201];
     }
     /// <summary>
-    /// 버튼 클릭에 대한 이벤트
+    /// 버튼 클릭에 대한 이벤트.
     /// </summary>
     public void BtnClick(ClickEvent _evt, Toggle _toggle, int _maxRange)
     {
@@ -279,8 +333,9 @@ public class MapEditors : MonoBehaviour
             }
         }
     }
-
-
+    /// <summary>
+    /// 생성한 Grid를 Json 방식으로 저장.
+    /// </summary>
     public void SaveBtn(Label _fileName, Label _result,  int _width, int _height)
     {
         if(btnTool == null || btnTool.childCount == 0)
@@ -292,63 +347,58 @@ public class MapEditors : MonoBehaviour
         //TODO Json으로 저장하는 방식 제작.
         int maxRange = _width * _height;
         Map maps = new Map();
-        maps.idx = new int[maxRange + 1, 2];
+        maps.idx = new int[maxRange, 2];
         float level = currentLevel > 100 ? ((float)currentLevel / 1000) : ((float)currentLevel / 100);
-        string str_lv = level >= 0 ?  new string("0") : level.ToString().Substring(level.ToString().IndexOf('.') + 1, 2);
-        Debug.Log(str_lv);
+        string str_lv = level == 0 ?  new string("00") : level.ToString().Substring(level.ToString().IndexOf('.') + 1, 2);
         maps.lv = currentLevel;
         maps.name = "Stage" + "_" + _width + "x" + _height + "_" + "0" + str_lv;
         _fileName.text = maps.name;
-        Debug.Log(maps.name);
         maps.width = _width;
         maps.height = _height;
         
-
         for(int i = 0; i < maxRange; i++)
         {
             maps.idx[i,0] = buttons[i].btnIconNum;
             maps.idx[i,1] = buttons[i].btnIconRotate;
         }
-        string jsonData = JsonUtility.ToJson(maps);
-        string path = Path.Combine(Application.dataPath + "/Resources/Maps", maps.name + ".json");
-        File.WriteAllText(path, jsonData);
+
+        string jsonData = JsonConvert.SerializeObject(maps);
+        CreateJsonFile(Application.dataPath + "/Resources/Maps", maps.name, jsonData);
 
         AssetDatabase.Refresh();
         _result.text = "저장 완료";
     }
-
+    /// <summary>
+    /// 저장된 Json을 다시 불러오기.
+    /// </summary>
     public void LoadBtn(Label _fileName, Label _result, BaseField<int> _currentlv, Toggle _toggle)
     {
-        if(btnTool != null || btnTool.childCount == 0)
-        {
-            var path = EditorUtility.OpenFilePanel("Open Level", Application.dataPath + 
-                    "/Resources/Maps", "json");
-            string filename = Path.GetFileName(path);
-            if(!string.IsNullOrEmpty(path))
-            {
-                LoadLVBtn(_fileName, _result, _currentlv, _toggle, path);
-                //currentLevelField.value = currentLevel;
-            }
-            _result.text = "불러오기 완료";
-        }
-        else
+        if(btnTool != null && btnTool.childCount != 0)
         {
             _result.text = "Grid를 생성 혹은 삭제 후 다시시도";
+            return;
         }
-        
+
+        var path = EditorUtility.OpenFilePanel("Open Level", Application.dataPath + 
+                    "/Resources/Maps", "json");
+        string filename = Path.GetFileName(path);
+        filename = filename.Substring(0, filename.ToString().IndexOf('.'));
+        if(!string.IsNullOrEmpty(path))
+        {
+            LoadLVBtn(_fileName, _result, _currentlv, _toggle, filename);
+        }
+        _result.text = "불러오기 완료";
     }
 
-    public void LoadLVBtn(Label _fileName, Label _result, BaseField<int> _currentlv, Toggle _toggle, string _path)
+    public void LoadLVBtn(Label _fileName, Label _result, BaseField<int> _currentlv, Toggle _toggle, string _filepath)
     {
-        Map maps = new Map();
+        Map maps = LoadJsonFile<Map>(Application.dataPath + "/Resources/Maps", _filepath);
 
-        string path = Path.Combine(_path);
-        string jsonData = File.ReadAllText(path);
-        maps = JsonUtility.FromJson<Map>(jsonData);
-        _fileName.text = maps.name;
-        _currentlv.value = maps.lv;
         // 버튼 생성
         BtnCreate(_toggle, _result, maps.width, maps.height);
+        _fileName.text = maps.name;
+        _currentlv.value = maps.lv;
+
         // grid 배치
         int maxRange = maps.width * maps.height;
         for (int i = 0; i < maxRange; i++)
@@ -358,6 +408,24 @@ public class MapEditors : MonoBehaviour
             buttons[i].btn.style.backgroundImage = new StyleBackground(iconData[maps.idx[i, 0]].icon);
             buttons[i].btn.style.rotate = new Rotate(new Angle(buttons[i].btnIconRotate * 90));
         }
+    }
+
+    private void CreateJsonFile(string _createPath, string _fileName, string _jsonData)
+    {
+        FileStream fileStream = new FileStream(string.Format("{0}/{1}.json", _createPath, _fileName), FileMode.Create);
+        byte[] data = Encoding.UTF8.GetBytes(_jsonData);
+        fileStream.Write(data, 0, data.Length);
+        fileStream.Close();
+    }
+
+    private T LoadJsonFile<T>(string _loadPath, string _fileName)
+    {
+        FileStream fileStream = new FileStream(string.Format("{0}/{1}.json", _loadPath, _fileName), FileMode.Open);
+        byte[] data = new byte[fileStream.Length];
+        fileStream.Read(data, 0, data.Length);
+        fileStream.Close();
+        string jsonData = Encoding.UTF8.GetString(data);
+        return JsonConvert.DeserializeObject<T>(jsonData);
     }
  
 }
